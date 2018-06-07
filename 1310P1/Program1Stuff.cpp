417a4b758f553fa1c821fa2251134c319d7a4200
Sam O'Heron <sjoheron42@students.tntech.edu>
2018-06-07 13:23:17 -0500

    Uploading from MacBook

---
+++ 1310P1/Program1Stuff.cpp
@@ -0,0 +1,9 @@
+// Create a function findIndex()
+// Call that function to run a binary search to find where the data belongs alphabetically
+// Returns index to tell you where to put your data
+// use namepsace CSC1310
+
+//understand the comparing and sorting
+//will read everything from a list and return a ListArray<CD>*
+//TEST EVERY SINGLE FUNCTION YOU WRITE
+//Use an iterator to walk through the list?
